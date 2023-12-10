bb1c760a680bd33c54fe9d9338f6f4b7597d331b
minjuko <rhalswn0526@gmail.com>
2023-12-10 12:05:47 +0900

    [Programmers-12978] 배달

--- MinJu/231204_week/Baekjoon_2240_자두나무.py
+++ MinJu/231204_week/Baekjoon_2240_자두나무.py
@@ -2,7 +2,7 @@ T, W = map(int, input().split())
 jadu = [0] + [int(input()) for _ in range(T)]
 dp = [[0] * (W + 1) for _ in range(T + 1)]
 
- dp[1][0], dp[1][1] = jadu[1] % 2, jadu[1] // 2
+dp[1][0], dp[1][1] = jadu[1] % 2, jadu[1] // 2
 for t in range(2, T + 1):
     for w in range(W + 1):
         j = jadu[t] % 2 if w % 2 == 0 else jadu[t] // 2
