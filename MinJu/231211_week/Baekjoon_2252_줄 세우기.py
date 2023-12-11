03e72704841eb9fe3cbd76613f5ca68b1bf49786
minjuko <rhalswn0526@gmail.com>
2023-12-11 16:15:59 +0900

    [Baekjoon-2252] 줄 세우기

---
+++ MinJu/231211_week/Baekjoon_2252_줄 세우기.py
@@ -0,0 +1,31 @@
+# 이산정렬 템플릿 적용 
+
+from collections import deque
+
+n, m = map(int, input().split())
+indegree = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1 
+
+def topology_sort():
+    result = []
+    q = deque()
+
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+    
+    while q:
+        now = q.popleft()
+        result.append(now)
+        for i in graph[now]:
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                q.append(i)
+    for i in result:
+        print(i, end=' ')
+topology_sort()
