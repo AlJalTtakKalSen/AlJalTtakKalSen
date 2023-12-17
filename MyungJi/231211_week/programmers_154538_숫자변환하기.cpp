f5af820f3760f5912558b2ccf52de1456ff28cd7
Starlight258 <audwl03071@gmail.com>
2023-12-17 23:59:19 +0900

    refactor: 프로그래머스 154538 문제 코드 리팩토링

--- MyungJi/231211_week/programmers_154538_숫자변환하기.cpp
+++ MyungJi/231211_week/programmers_154538_숫자변환하기.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 #define INF 1e9
+#define MX 2000004
 using namespace std;
-int dp[2000004];
+int dp[MX];
 int solution(int x, int y, int n) {
-    int answer = 0;
-    fill (dp, dp+2000004, INF);
+    fill (dp, dp+MX, INF);
     dp[x]=0;
     for (int i=x;i<=y;i++){
-        if (i + n < 2000004) dp[i + n] = min(dp[i + n], dp[i] + 1);
-        if (i * 2 < 2000004) dp[i*2] = min(dp[i]+1, dp[i*2]);
-        if (i * 3 < 2000004) dp[i*3] = min(dp[i]+1, dp[i*3]);
+        if (i + n < MX) dp[i + n] = min(dp[i + n], dp[i] + 1);
+        if (i * 2 < MX) dp[i*2] = min(dp[i]+1, dp[i*2]);
+        if (i * 3 < MX) dp[i*3] = min(dp[i]+1, dp[i*3]);
     }
     if (dp[y]==INF) return -1;
     else return dp[y];
