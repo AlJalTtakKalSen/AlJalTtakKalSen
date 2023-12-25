e28b00a1d0868a733550f4ca0f88bd3c6f12d62f
황수연 <hwangsooyeon@hwangsooyeon-ui-MacBookAir.local>
2023-12-25 20:21:12 +0900

    [Baekjoon-1300] K번째 수

--- SooYeon/231225_week/Baekjoon_1300_k번째 수.js
+++ SooYeon/231225_week/Baekjoon_1300_k번째 수.js
@@ -17,7 +17,28 @@ while(start <= end) {
   if(total < k) start = mid + 1;
   else {
     result = mid;
-    end = mid - 1;
+    end = mid - 1;let fs = require('fs');
+    let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+    
+    let n = Number(input[0]);
+    let k = Number(input[1]);
+    
+    let start = 1; // 가장 작은 값
+    let end = 10 ** 9; // 가장 큰 값
+    
+    let result = 0;
+    while(start <= end) {
+      let mid = parseInt((start + end) / 2); // 찾고자 하는 B[k]
+      let total = 0;
+      for(let i = 1; i <= n; i++) {
+        total += Math.min(parseInt(mid / i), n); // mid보다 작은 수의 개수
+      }
+      if(total < k) start = mid + 1;
+      else {
+        result = mid;
+        end = mid - 1;
+      }
+    }
   }
 }
 
