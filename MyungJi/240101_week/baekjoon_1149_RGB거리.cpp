e2ebb907654a085202b42d57e81954e90eb1e3ee
Starlight258 <audwl03071@gmail.com>
2024-01-05 00:38:15 +0900

    refactor: RGB 코드 리팩토링

--- MyungJi/240101_week/baekjoon_1149_RGB거리.cpp
+++ MyungJi/240101_week/baekjoon_1149_RGB거리.cpp
@@ -1,31 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
+int n;
+int cost[1004][3], dp[1004][3];
 
-int main() {
-    int N;
-    cin >> N;
-
-    vector<vector<int>> cost(N, vector<int>(3));
-    vector<vector<int>> dp(N, vector<int>(3));
-
-    for (int i = 0; i < N; ++i) {
-        cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
+int main(){
+    //1. 입력받기
+    cin >> n;
+    for (int i=0;i<n;i++)
+        for (int j=0;j<3;j++) 
+            cin >> cost[i][j];
+    //2. dp 수행
+    dp[0][0] = cost[0][0], dp[0][1] = cost[0][1], dp[0][2] = cost[0][2];
+    for(int i=1;i<n;i++){
+        dp[i][0] += min(dp[i-1][1], dp[i-1][2]) + cost[i][0];
+        dp[i][1] += min(dp[i-1][0], dp[i-1][2]) + cost[i][1];
+        dp[i][2] += min(dp[i-1][0], dp[i-1][1]) + cost[i][2];
     }
-
-    dp[0][0] = cost[0][0];
-    dp[0][1] = cost[0][1];
-    dp[0][2] = cost[0][2];
-
-    for (int i = 1; i < N; ++i) {
-        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
-    }
-
-    cout << min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]}) << endl;
-
-    return 0;
+    //3. 정답 출력
+    cout << min({dp[n-1][0], dp[n-1][1], dp[n-1][2]});
 }
