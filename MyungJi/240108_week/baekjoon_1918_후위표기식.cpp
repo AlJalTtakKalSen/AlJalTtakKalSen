2f09ae917ffa5e6a9508f86e7aa6c9ff1ecd33ab
Starlight258 <audwl03071@gmail.com>
2024-01-09 00:00:33 +0900

    refactor: 리팩토링

--- MyungJi/240108_week/baekjoon_1918_후위표기식.cpp
+++ MyungJi/240108_week/baekjoon_1918_후위표기식.cpp
@@ -1,16 +1,12 @@
-#include <iostream>
-#include <stack>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-// Function to return precedence of operators
 int precedence(char c) {
     if(c == '*' || c == '/') return 2;
     else if(c == '+' || c == '-') return 1;
     else return -1;
 }
 
-// Function to convert infix expression to postfix
 string infixToPostfix(string s) {
     stack<char> st; 
     string result;
@@ -18,25 +14,20 @@ string infixToPostfix(string s) {
     for(int i = 0; i < s.length(); i++) {
         char c = s[i];
 
-        // If the character is an operand, add it to the output
         if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
             result += c;
 
-        // If the character is an '(', push it to the stack
         else if(c == '(')
             st.push('(');
 
-        // If the character is an ')', pop and output from the stack 
-        // until an '(' is encountered
         else if(c == ')') {
             while(st.top() != '(') {
                 result += st.top();
                 st.pop();
             }
-            st.pop(); // Pop the '('
+            st.pop();
         }
 
-        // If the character is an operator
         else {
             while(!st.empty() && precedence(s[i]) <= precedence(st.top())) {
                 result += st.top();
@@ -46,7 +37,6 @@ string infixToPostfix(string s) {
         }
     }
 
-    // Pop all the remaining elements from the stack
     while(!st.empty()) {
         result += st.top();
         st.pop();
