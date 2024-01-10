0482895a8decb80fda202005b4ac51d7c0719ed4
Starlight258 <audwl03071@gmail.com>
2024-01-11 00:51:47 +0900

    [Baekjoon-1932] 정수삼각형 코드 리팩토링

--- MyungJi/240108_week/baekjoon_1932_정수삼각형.cpp
+++ MyungJi/240108_week/baekjoon_1932_정수삼각형.cpp
@@ -1,30 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int tri[504][504];
-int n;
-long long answer;
-long long dp[504][504];
+int n, answer;
 
 int main(){
     //1. 입력받기
     cin >> n;
     for (int i=0;i<n;i++){
-        for(int j=0;j<i+1;j++){
+        for(int j=0;j<=i;j++){
             cin >> tri[i][j];
         }
     }
     //2. 합 구하기
-    dp[0][0]= tri[0][0];
-    for (int i=0;i<n-1;i++){
-        for (int j=0;j<i+1;j++){ 
-            dp[i+1][j] = dp[i][j] + tri[i+1][j];
-            dp[i+1][j+1] = dp[i][j] + tri[i+1][j+1];
+    for (int i=n-2;i>=0;i--){
+        for (int j=0;j<=i;j++){ 
+            tri[i][j] = tri[i][j] + max(tri[i+1][j], tri[i+1][j+1]);
         }
     }
-    //3. 답 구하기
-    for (int i=0;i<n;i++){
-        if (answer<dp[n-1][i]) answer = dp[n-1][i];
-    }
-    //4. 정답 출력
-    cout << answer;
+    //3. 정답 출력하기
+    cout << tri[0][0];
 }
+
