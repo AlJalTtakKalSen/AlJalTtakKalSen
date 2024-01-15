f70ee733cc9838ad7262d039a62a5670a13f07b5
sooobb <sb3274@naver.com>
2024-01-15 14:28:49 +0900

    [Baekjoon-1697] 숨바꼭질

---
+++ SooBin/240115_week/Baekjoon_1697_숨바꼭질.cpp
@@ -0,0 +1,57 @@
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<climits>
+
+#define MAX 9999999
+
+using namespace std;
+
+int main() 
+{
+    int N, K;
+    cin >> N >> K;
+    bool visited[100001] = { false };
+
+    queue<pair<int, int>> q;
+    visited[N] = true;
+    q.push(make_pair(N,0));
+
+    while (!q.empty())
+    {
+        int location = q.front().first;
+        int time = q.front().second;
+        q.pop();
+
+        int case1 = location * 2;
+        int case2 = location + 1;
+        int case3 = location - 1;
+
+        if (location == K)
+        {
+            cout << time;
+            break;
+        }
+       
+        if (case1 >= 0 && case1 <= 100001 && !visited[case1])
+        {
+            visited[case1] = true;
+            q.push(make_pair(case1, time + 1));
+        }
+        if (case2 >= 0 && case2 <= 100001 && !visited[case2])
+        {
+            visited[case2] = true;
+            q.push(make_pair(case2, time + 1));
+        }
+        if (case3 >= 0 && case3 <= 100001 && !visited[case3])
+        {
+            visited[case3] = true;
+            q.push(make_pair(case3, time + 1));
+        }
+
+    }
+
+    return 0;
+}
