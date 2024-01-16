a3938ac91f757009c0bc6174f0419aa21cf2908b
Starlight258 <audwl03071@gmail.com>
2024-01-16 23:13:04 +0900

    [Baekjoon-2743] 단어 길이 재기 java로 푼 첫 문제

---
+++ MyungJi/230116_week/baekjoon_2743_단어길이재기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Baekjoon_2743_단어길이재기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input.length());
+    }
+}
