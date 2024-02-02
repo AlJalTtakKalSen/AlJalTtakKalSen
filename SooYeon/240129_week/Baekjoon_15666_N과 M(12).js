da63cc1768eb0dc225c0c8a85f6bd3d595fd2bb9
H-sooyeon <wndqhr__@naver.com>
2024-02-02 23:17:38 +0900

    rename: Baekjoon-16953

--- SooYeon/240129_week/Baekjoon_15666_N과 M(12).js
+++ SooYeon/240129_week/Baekjoon_15666_N과 M(12).js
@@ -1,8 +1,8 @@
-// let fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let input = `4 2
-9 7 9 1`.split('\n');
+// let input = `4 2
+// 9 7 9 1`.split('\n');
 
 let [n, m] = input[0].split(' ').map(Number);
 let arr = input[1].split(' ').map(Number);
