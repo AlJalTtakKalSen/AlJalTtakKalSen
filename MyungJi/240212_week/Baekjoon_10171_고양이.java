27f2cca5760707dda2bed4c6a3443b6a4d672b0d
Starlight258 <audwl03071@gmail.com>
2024-02-13 23:20:08 +0900

    [Baekjoon-10171] 고양이

---
+++ MyungJi/240212_week/Baekjoon_10171_고양이.java
@@ -0,0 +1,12 @@
+public class prac10 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|\n");
+
+        System.out.println(sb.toString());
+    }
+
+}
