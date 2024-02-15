bd0b23659cf3cb6b7c1d7e4ee19b5473183f075a
Mint <audwl03071@gmail.com>
2024-02-15 23:28:30 +0900

    [Baekjoon-10172] 개

---
+++ MyungJi/240212_week/Baekjoon_10172_개.java
@@ -0,0 +1,11 @@
+public class prac12 {
+
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+
+}
